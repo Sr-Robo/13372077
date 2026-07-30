91d4dd525f6feb3f092ceeaa0bb7378f40a2bb9c
fxlip <felip42@gmail.com>
2026-07-30 15:36:20 -0300

    fix: remove Footer.jsx from body area, it breaks CartProvider same as Header

--- dist/pages/all/Footer.d.ts
+++
@@ -1,9 +0,0 @@
-export default function Footer({ themeConfig }: {
-    themeConfig: any;
-}): React.JSX.Element;
-export namespace layout {
-    let areaId: string;
-    let sortOrder: number;
-}
-export const query: "\n  query query {\n    themeConfig {\n      copyRight\n    }\n  }\n";
-import React from 'react';
